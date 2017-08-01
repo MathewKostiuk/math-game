b69285435351fe5609843f1e1247337219f7be76
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-01 04:04:19 +0000

    implemented game

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require './player'
 require './questions'
 require './master'
-arr = []
-frank = MathGame::Player.new('player', 3)
-arr << frank
+
+MathGame::Master.new
--- master.rb
+++ master.rb
@@ -6,6 +6,7 @@ module MathGame
       @game = 'on'
       @player_1 = MathGame::Player.new('Player 1', 3)
       @player_2 = MathGame::Player.new('Player 2', 3)
+      @players = [@player_1, @player_2]
       @question = MathGame::Question.new
       puts 'Welcome to Math-a-palooza! Where Math === FUN!'
       while @game == 'on'
@@ -15,6 +16,9 @@ module MathGame
           puts 'GAME OVER --- TIE GAME'
           @game = 'game-over'
         else
+          winner = @players.max_by(&:lives)
+          puts "Congratulations, #{winner.name}. U R t3h ub3r m47h l0rdz"
+          @game = 'game-over'
         end
       end
     end
@@ -26,16 +30,16 @@ module MathGame
         player.lives -= 1
         puts "Yo' noggin still workin', Bingo?"
       end
-      puts "Current Score: #{@player_1.lives}/3 VS. #{@player_2.lives}/3"
+      puts "Current Score:#{@player_1.name}: #{@player_1.lives}/3 lives left VS. #{@player_2.name}: #{@player_2.lives}/3 lives left"
 
     end
 
     def new_turn
       puts "----- NEXT ROUND BEGINNING -----"
       @question.new_question(@player_1.name)
-      handle_answer(@player_1.name, gets.chomp)
+      handle_answer(@player_1, gets.chomp)
       @question.new_question(@player_2.name)
-      handle_answer(@player_2.name, gets.chomp)
+      handle_answer(@player_2, gets.chomp)
     end
 
   end
