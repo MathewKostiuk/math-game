d275f738e2eb1a28eda8eb90236cfcf8448d2c59
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-01 04:36:28 +0000

    implemented ability to choose custom name

--- player.rb
+++ player.rb
@@ -2,12 +2,22 @@ module MathGame
 
   class Player
 
-    attr_accessor :name
-    attr_accessor :lives
+    DEFAULT_NAMES = ['Player 1', 'Player 2']
+    @@total_players = 0
+
+    attr_accessor :name, :lives
 
     def initialize(name, lives)
-      @name = name
+      @@total_players += 1
       @lives = lives
+      puts '----- TYPE IN YOUR NAME(OPTIONAL) -----'
+      test = gets.strip
+      if test.empty? && @@total_players == 1
+        test = DEFAULT_NAMES[0]
+      elsif test.empty? && @@total_players == 2
+        test = DEFAULT_NAMES[1]
+      end
+      @name = test
     end
 
   end
